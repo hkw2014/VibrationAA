c160fc9c7901d88220f78127724ecdfbdf108760
KeishiroOsaki <startipend2008@gmail.com>
2014-11-14 21:32:48 +0900

    no message

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
 (function() {
   chrome.browserAction.onClicked.addListener(function() {
     alert("韓国吸収！");
-    return chrome.tabs.executeScript(null, {
+    chrome.tabs.executeScript(null, {
       "code": "document.body.innerHTML = " + "document.body.innerHTML.replace(/韓国/g,'朝鮮');"
+    });
+    return chrome.tabs.executeScript(null, {
+      "code": "document.body.innerHTML = " + "document.body.innerHTML.replace(/北朝鮮/g,'朝鮮');"
     });
   });
 
